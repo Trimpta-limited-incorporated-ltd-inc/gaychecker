97ae11fdc05d69ec7e79bd4d5f3cfcb45db56332
Trimpta <68428753+trimpta@users.noreply.github.com>
2024-03-02 16:28:31 +0530

    make

---
+++ scripts.js
@@ -0,0 +1,18 @@
+straigtlist = ["trimpta", "august"]
+
+function testResults (form) {
+    var name = form.inputbox.value;
+    
+    let results = document.getElementById("reslt");
+
+    if (name != ""){
+    if (straigtlist.includes(name.toLowerCase())) {
+        results.textContent = name.toUpperCase() + " IS NOT GAY"
+    }
+    else {
+        results.textContent = name.toUpperCase() + " IS GAY"
+    }
+}
+
+
+}
